57819719220c57b2764274b13b7f414e0b7e1001
Seu Nome <108072743+jadsonogueira@users.noreply.github.com>
2024-11-10 14:52:20 -0500

    Resolve merge conflicts

--- public/script.js
+++ public/script.js
@@ -46,7 +46,8 @@ const fluxoInstrucoes = {
   'Consultar empenho': 'Por favor, preencha todos os campos. Certifique-se de selecionar o contrato SEI correto da lista disponível. Após o processamento, você receberá um email com o resultado da pesquisa',
   'Liberar assinatura externa': 'Por favor, preencha todos os campos. O número do DOC_SEI deve ser informado no formato numérico (exemplo: 12345678).',
   'Liberar acesso externo': 'Por favor, preencha todos os campos. O número do processo SEI deve seguir o formato: 50600.001234/2024-00.',
-  'Alterar ordem de documentos': 'Por favor, preencha todos os campos. No campo de instruções, descreva detalhadamente a ordem desejada dos documentos na árvore do processo SEI digitado.'
+  'Alterar ordem de documentos': 'Por favor, preencha todos os campos. No campo de instruções, descreva detalhadamente a ordem desejada dos documentos na árvore do processo SEI digitado.',
+  'Inserir anexo em doc SEI': 'Por favor, preencha todos os campos. O número do DOC_SEI deve ser informado no formato numérico (exemplo: 12345678).'
 };
 
 // Função para abrir o formulário de acordo com o fluxo selecionado
