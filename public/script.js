6c3f8e76ce9dbbc3ca57029e6b9e9142aa8226fc
jadsonogueira <108072743+jadsonogueira@users.noreply.github.com>
2024-11-12 18:27:40 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -42,11 +42,12 @@ const listacontratos = [
 
 // Objeto com instruções específicas para cada fluxo
 const fluxoInstrucoes = {
-  'Consultar empenho': 'Por favor, preencha todos os campos. Certifique-se de selecionar o contrato SEI correto da lista disponível. Após o processamento, você receberá um email com o resultado da pesquisa',
-  'Liberar assinatura externa': 'Por favor, preencha todos os campos. O número do DOC_SEI deve ser informado no formato numérico (exemplo: 12345678). Envie uma solicitação para cada documento.',
-  'Liberar acesso externo': 'Por favor, preencha todos os campos. O número do processo SEI deve seguir o formato: 50600.001234/2024-00.',
-  'Alterar ordem de documentos': 'Por favor, preencha todos os campos. No campo de instruções, descreva detalhadamente a ordem desejada dos documentos na árvore do processo SEI digitado.'
+  'Consultar empenho': 'Por favor, preencha todos os campos. **Certifique-se de selecionar o contrato SEI correto** da lista disponível. Após o processamento, você receberá um email com o resultado da pesquisa.',
+  'Liberar assinatura externa': 'Por favor, preencha todos os campos. **O número do DOC_SEI deve ser informado no formato numérico** (exemplo: 12345678). **Envie uma solicitação para cada documento**.',
+  'Liberar acesso externo': 'Por favor, preencha todos os campos. **O número do processo SEI deve seguir o formato: 50600.001234/2024-00**.',
+  'Alterar ordem de documentos': 'Por favor, preencha todos os campos. **No campo de instruções, descreva detalhadamente a ordem desejada** dos documentos na árvore do processo SEI digitado.'
 };
+
 
 // Função para abrir o formulário de acordo com o fluxo selecionado
 function abrirFormulario(fluxo) {
