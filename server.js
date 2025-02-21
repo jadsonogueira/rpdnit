5dcfa3d2c66dc49739586088502431418de6ff9b
jadsonogueira <108072743+jadsonogueira@users.noreply.github.com>
2025-02-21 17:23:26 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ const cors = require('cors');
 const path = require('path');
 const multer = require('multer');
 const AdmZip = require('adm-zip');
-const { fromBuffer } = require("pdf2pic");
 
 const app = express();
 app.use(cors());
+
+// Configuração para processar dados do tipo application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+// Verificação de variáveis de ambiente essenciais
 if (
   !process.env.MONGODB_URL ||
   !process.env.JWT_SECRET ||
@@ -24,6 +26,7 @@ if (
   process.exit(1);
 }
 
+// Conexão ao MongoDB
 mongoose
   .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
@@ -35,33 +38,46 @@ mongoose
     process.exit(1);
   });
 
+// Definir o esquema do usuário
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
+
 const User = mongoose.model('User', userSchema);
+
+// Servir arquivos estáticos da pasta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Rota para testar a conexão com o MongoDB
 app.get('/test-db', (req, res) => {
   res.send('Conexão com o MongoDB funcionando.');
 });
 
+// Rota para Signup
 app.post('/signup', express.json(), async (req, res) => {
   try {
     const { username, email, password } = req.body;
+
     if (!username || !email || !password) {
       return res.status(400).send('Todos os campos são obrigatórios');
     }
+
+    // Verifica se o usuário ou email já existe
     const existingUser = await User.findOne({
       $or: [{ username }, { email }],
     });
     if (existingUser) {
       return res.status(400).send('Usuário ou e-mail já cadastrado');
     }
+
     const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Criar o novo usuário no banco de dados
     const user = new User({ username, email, password: hashedPassword });
     await user.save();
+
     res.status(201).send('Usuário registrado com sucesso');
   } catch (err) {
     console.error('Erro ao registrar usuário:', err);
@@ -69,18 +85,23 @@ app.post('/signup', express.json(), async (req, res) => {
   }
 });
 
+// Rota para Login
 app.post('/login', express.json(), async (req, res) => {
   try {
     const { username, password } = req.body;
+
     if (!username || !password) {
       return res.status(400).send('Todos os campos são obrigatórios');
     }
+
     const user = await User.findOne({ username });
     if (!user) {
       return res.status(400).send('Usuário não encontrado');
     }
+
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).send('Senha incorreta');
+
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: '1h',
     });
@@ -91,22 +112,28 @@ app.post('/login', express.json(), async (req, res) => {
   }
 });
 
+// Configuração do Multer para aceitar múltiplos arquivos
 const upload = multer({
   storage: multer.memoryStorage(),
-  limits: { fileSize: 50 * 1024 * 1024 },
+  limits: {
+    fileSize: 50 * 1024 * 1024, // Limite de 50MB por arquivo
+  },
 });
 
+// Rota para envio de e-mails
 app.post('/send-email', upload.any(), async (req, res) => {
   try {
     const fluxo = req.body.fluxo;
     const dados = req.body;
+
     if (!dados.email) {
       return res.status(400).send('O campo de e-mail é obrigatório.');
     }
+
     let mailContent = `Fluxo: ${fluxo}\n\nDados do formulário:\n`;
     mailContent += `Requerente: ${dados.requerente || ''}\n`;
     mailContent += `Email: ${dados.email || ''}\n`;
-    
+
     if (fluxo === 'Liberar assinatura externa') {
       mailContent += `Assinante: ${dados.assinante || ''}\n`;
       mailContent += `Número do DOC_SEI: ${dados.numeroDocSei || ''}\n`;
@@ -131,13 +158,18 @@ app.post('/send-email', upload.any(), async (req, res) => {
       mailContent += `Tipo do Documento: ${dados.tipoDocumento || ''}\n`;
       mailContent += `Número: ${dados.numero || ''}\n`;
       mailContent += `Nome na Árvore: ${dados.nomeArvore || ''}\n`;
+      
     } else if (fluxo === 'Criar Doc SEI Externo') {
+     // Obtém a data atual e ajusta o fuso horário (UTC-3 para horário de Brasília)
       const agora = new Date();
-      agora.setHours(agora.getHours() - 3);
+      agora.setHours(agora.getHours() - 3); // Ajusta o fuso horário para UTC-3
+    
       const dia = String(agora.getDate()).padStart(2, '0');
       const mes = String(agora.getMonth() + 1).padStart(2, '0');
       const ano = agora.getFullYear();
       const dataFormatada = `${dia}/${mes}/${ano}`;
+    
+      // Adiciona as informações ao conteúdo do e-mail
       mailContent += `Número do Processo SEI: ${dados.processoSei || ''}\n`;
       mailContent += `Data: ${dataFormatada}\n`;
       mailContent += `Tipo do Documento: ${dados.tipoDocumento || ''}\n`;
@@ -145,90 +177,103 @@ app.post('/send-email', upload.any(), async (req, res) => {
       mailContent += `Nome na Árvore: ${dados.nomeArvore || ''}\n`;
     }
     
+   
     const transporter = nodemailer.createTransport({
       service: 'gmail',
-      auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
     });
-    
+
     const mailOptions = {
       from: process.env.EMAIL_USER,
-      to: 'jadson.pena@dnit.gov.br',
+      to: 'jadson.pena@dnit.gov.br', // Ajuste o destinatário conforme necessário
       subject: `${fluxo}`,
       text: mailContent,
     };
-    
+
     const attachments = [];
-    
+
+    // Processar os arquivos enviados
     if (req.files && req.files.length > 0) {
       for (const file of req.files) {
         if (file.fieldname.startsWith('imagem')) {
+          // Arquivos de imagem individuais
+          // Validar o tipo de arquivo
           if (!file.mimetype.startsWith('image/')) {
             return res.status(400).send(`Tipo de arquivo não permitido: ${file.originalname}`);
           }
+          // Validar o tamanho do arquivo (limite de 5MB)
           if (file.size > 5 * 1024 * 1024) {
             return res.status(400).send(`Arquivo muito grande: ${file.originalname}`);
           }
-          attachments.push({ filename: file.originalname, content: file.buffer });
+          // Adicionar aos anexos
+          attachments.push({
+            filename: file.originalname,
+            content: file.buffer,
+          });
         } else if (file.fieldname === 'arquivoZip') {
+          // Arquivo ZIP
           try {
             const zip = new AdmZip(file.buffer);
             const zipEntries = zip.getEntries();
+
+            // Verificar se há mais de 100 arquivos (somando com os individuais)
             if (attachments.length + zipEntries.length > 100) {
               return res.status(400).send('O total de arquivos excede o limite de 100.');
             }
+
             for (const entry of zipEntries) {
+              // Ignorar diretórios
               if (entry.isDirectory) continue;
+
+              // Validar o tipo de arquivo
               const extension = path.extname(entry.entryName).toLowerCase();
               const allowedExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp'];
               if (!allowedExtensions.includes(extension)) {
                 return res.status(400).send(`Tipo de arquivo não permitido no ZIP: ${entry.entryName}`);
               }
+
+              // Extrair o conteúdo do arquivo
               const fileContent = entry.getData();
+
+              // Validar o tamanho do arquivo (limite de 5MB)
               if (fileContent.length > 5 * 1024 * 1024) {
                 return res.status(400).send(`Arquivo muito grande no ZIP: ${entry.entryName}`);
               }
-              attachments.push({ filename: entry.entryName, content: fileContent });
+
+              // Adicionar aos anexos
+              attachments.push({
+                filename: entry.entryName,
+                content: fileContent,
+              });
             }
           } catch (error) {
             console.error('Erro ao processar o arquivo ZIP:', error);
             return res.status(400).send('Erro ao processar o arquivo ZIP.');
           }
         } else if (file.fieldname === 'arquivo') {
-          attachments.push({ filename: file.originalname, content: file.buffer });
-        } else if (file.fieldname === 'arquivoPdf') {
-          try {
-            const pdfOptions = {
-              density: 150,
-              format: "jpg",
-              width: 1240,
-              height: 1754
-            };
-            const converter = fromBuffer(file.buffer, pdfOptions);
-            const convertedPages = await converter.bulk(-1);
-            for (const pageResult of convertedPages) {
-              const imageBuffer = Buffer.from(pageResult.base64, 'base64');
-              attachments.push({
-                filename: `${file.originalname.replace(/\.pdf$/i, '')}_page_${pageResult.page}.jpg`,
-                content: imageBuffer
-              });
-            }
-          } catch (error) {
-            console.error("Erro na conversão de PDF para JPG:", error);
-            return res.status(400).send("Erro na conversão do PDF para JPG.");
-          }
+          // Outros arquivos (por exemplo, para 'Inserir anexo em doc SEI')
+          attachments.push({
+            filename: file.originalname,
+            content: file.buffer,
+          });
         }
       }
     }
-    
+
+    // Verificar se há anexos para adicionar
     if (attachments.length > 0) {
       mailOptions.attachments = attachments;
     }
-    
+
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
         console.error('Erro ao enviar o e-mail:', error);
         return res.status(500).send('Erro ao enviar o e-mail');
       }
+
       res.send('E-mail enviado com sucesso');
     });
   } catch (err) {
@@ -237,9 +282,11 @@ app.post('/send-email', upload.any(), async (req, res) => {
   }
 });
 
+// Servir a página inicial (dashboard.html) ao acessar a rota raiz
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
 });
 
+// Inicia o servidor
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
