cf52c355223196ec94c24596bf533856a882f45b
jadsonogueira <108072743+jadsonogueira@users.noreply.github.com>
2025-06-05 17:05:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email:    { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role:     { type: String, enum: ['user', 'admin'], default: 'user' }
+  role:     { type: String, enum: ['user', 'admin'], default: 'user' },
 });
 const User = mongoose.model('User', userSchema);
 
