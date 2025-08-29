15007769089d03dc1e6ab83ed52e910023f9239e
jadsonogueira <108072743+jadsonogueira@users.noreply.github.com>
2025-08-29 15:43:47 -0500

    Update server.js

--- server.js
+++ server.js
@@ -883,7 +883,7 @@ app.post('/send-email', upload.any(), async (req, res) => {
           // Anexa o PDF (ou qualquer arquivo) sem compressão
           attachments.push({ filename: safeOriginalName, content: file.buffer });
         
-       } } else if (file.fieldname === 'arquivoPdf') {
+       } else if (file.fieldname === 'arquivoPdf') {
   const deveConverterPDF = ['Criar Doc SEI Editável', 'Inserir imagem em doc SEI', 'PDF para JPG'].includes(fluxo);
 
   try {
